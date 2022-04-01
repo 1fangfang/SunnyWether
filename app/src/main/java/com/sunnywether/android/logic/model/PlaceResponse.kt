b31fe2d97e5cd8b23fa2b8ad2b26f4a8b7c727c9
Fang <2292691928@qq.com>
2022-04-01 17:32:33 +0800

    Step_2
    定义数据模型

---
+++ app/src/main/java/com/sunnywether/android/logic/model/PlaceResponse.kt
@@ -0,0 +1,13 @@
+package com.sunnywether.android.logic.model
+
+import com.google.gson.annotations.SerializedName
+
+/*
+定义数据模型
+ */
+data class PlaceResponse(val status: String, val place: List<Place>) // 返回的地址数据：状态，城市列表
+
+data class Place(val name: String, val location: Location,
+                 @SerializedName("formatted_address") val address: String) // 城市名字，坐标,地址
+
+data class Location(val lng: String, val lat:String) // 横纵坐标
