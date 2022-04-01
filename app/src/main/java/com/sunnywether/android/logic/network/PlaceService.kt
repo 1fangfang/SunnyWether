f0bb3989396c52e812b8d8d6f7bdae56933e9484
Fang <2292691928@qq.com>
2022-04-01 18:53:30 +0800

    Step_3
    1.城市搜索接口
    2.Retrofit构建器优化
    3.使用协程封装网络请求API

---
+++ app/src/main/java/com/sunnywether/android/logic/network/PlaceService.kt
@@ -0,0 +1,17 @@
+package com.sunnywether.android.logic.network
+
+import com.sunnywether.android.SunnyWeatherApplication
+import com.sunnywether.android.logic.model.PlaceResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/*
+retrofit接口
+ */
+
+
+interface PlaceService {
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+}
